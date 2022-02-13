e14e4cdbe0221323ddde9dca3e8ad3920f6b2b61
Oshomaru <osh.malr3i60@gmail.com>
2022-02-13 19:05:41 +0900

    work1

--- 03_サンプル/01_カウンター/main.js
+++ 03_サンプル/01_カウンター/main.js
@@ -1,12 +1,24 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const mainasButton = document.getElementById("mainas-button")
+const baiButton = document.getElementById("bai-button")
 
 let count = 0
 
 // ボタンをクリックしたときの処理を登録
 plusButton.onclick = function () {
   // count を更新
-  count += 1
+  count += 2
   // count を表示
+  display.textContent = count
+}
+
+mainasButton.onclick = function () {
+  count -= 2
+  display.textContent = count
+}
+
+baiButton.onclick = function () {
+  count *= 2
   display.textContent = count
 }
